8ad28f966cb021d5f70c974b6028f9ef532eadad
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 18:13:40 +0300

    Сохранила задачу 2

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// === Задача 1: Переводим десятеричное число в двоичное
+
+/* string ChangeNumber(int number)
+{
+    string result = string.Empty;    
+    while(number >= 1)
+    {
+    // пусто = новый остаток "number % 2" приклеили к предыдущему остатку "result" - и так по кругу
+        result = number % 2 + result;
+        number /= 2; // аналогично number = number / 2;
+    }
+    return result;
+}
+
+Console.WriteLine(ChangeNumber(10)); */
+
+// === Задача 2: Напишите программу, которая перевернет одномерный массив
+// последний будет на первом, а первый на последнем
+
+int CreateRandomArr(int size, int min, int max) // метод, который преобразует и выводит рандомный массив на экран
+{
+    int[] arr = new int[size];  
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min, max +1);
+    }
+    return arr;
+}
+
+void ShowArr (int[] arr) // это для чего???
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] ReversArr (int[] arr) // метод, который переворачивает массив
+{
+    int j = arr.Length - 1;
+    int temp = 0;
+    for(int i = 0; i < arr.Length/2; i++)
+    {
+        temp = arr[1];
+        arr[i] = arr[j];
+        arr [j] = temp;
+        j--;
+    }
+    return arr;
+}
+
+int[] myArray = CreateRandomArr(5, 1, 10);
+ShowArr(myArray);
+ShowArr(ReversArr(myArray));
+
+
+
+
+// === Задача 3: Напишите программу, которая принимает 3 числа и проверяет может ли существовать 
+// треугольник с такими длинами сторон.
+// Правило: каждая из сторон должны быть строго меньше суммы 2-х других
+
+// === Задача 4: Без применения рекурсии, написать метод, который выведет "n" чисел Фибоначчи
+// необходимо запрашивать и первые 2 числа Фибоначчи.
+
+// 3 3 6 9 15 24 39, n = 24 + 39 и т.д.
+
+
