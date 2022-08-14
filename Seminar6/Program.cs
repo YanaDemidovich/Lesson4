5a0eadf1850ea35e4df277107fadcf92557fe801
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 19:02:01 +0300

    Сохранила задачу 4

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -58,9 +58,9 @@ ShowArr(ReversArr(myArray)); */
 
 // === Задача 3: Напишите программу, которая принимает 3 числа и проверяет может ли существовать 
 // треугольник с такими длинами сторон.
-// a < b + c
+// Правило: каждая из сторон должны быть строго меньше суммы 2-х других, а именно: a < b + c
 
-bool Checking (int dot1, int dot2, int dot3)
+/* bool Checking (int dot1, int dot2, int dot3) // метод проверки сторон правилу a < b + c
 {
     if (dot1 < dot2 + dot3 && dot2 < dot1 + dot3 && dot3 < dot1 + dot2)
     {
@@ -76,14 +76,39 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Длина третьей стороны ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Checking(num1, num2, num3));
-
-
-// Правило: каждая из сторон должны быть строго меньше суммы 2-х других
+Console.WriteLine(Checking(num1, num2, num3)); */
 
 // === Задача 4: Без применения рекурсии, написать метод, который выведет "n" чисел Фибоначчи
 // необходимо запрашивать и первые 2 числа Фибоначчи.
-
 // 3 3 6 9 15 24 39, n = 24 + 39 и т.д.
 
+/* int[] Fibonachi (int numForm1, int numForm2, int dlina) // метод вычисления формулы Фиббоначи
+{
+    int[] array = new int[dlina];
+    array[0] = numForm1;
+    array[1] = numForm2;
+    for (int i = 2; i < dlina; i++)
+    {
+        array[i] = array[i - 2] + array[i - 1];
+    }
+    return array;
+}
 
+void ShowArr (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите первое число начала Фиббоначи ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число начала Фиббоначи ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину Фиббоначи ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Fibonachi(num1, num2, num3)); */
+// на выходе выдает ответ "System.Int32[]" - не знаю в чем проблема. Хотя на семинаре у студента всё работало
